fec2eafd46d296bc05c9449f4ae60900dec12deb
dohehehe <doheerlob@gmail.com>
2025-11-02 16:40:39 +0900

    ✨ feat: Credit 게시물 기능 추가

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,7 +1,13 @@
+'use client';
+
 import IconBoard from '@/components/admin/IconBoard';
+import CreditBoard from '@/components/admin/CreditBoard';
 
 export default function AdminPage() {
   return (
-    <IconBoard />
+    <>
+      <IconBoard />
+      <CreditBoard />
+    </>
   );
 }
