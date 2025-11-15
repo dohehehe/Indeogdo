636b9b797247f50e774092cccf034aac7ef0ffe1
dohehehe <doheerlob@gmail.com>
2025-11-15 18:19:05 +0900

    💄style: MarRest 아이콘 변경

--- src/components/Map/MapReset.jsx
+++ src/components/Map/MapReset.jsx
@@ -3,14 +3,14 @@ import { theme } from '@/styles/Theme';
 
 const MapResetButton = styled.button`
   position: fixed;
-  top: 65px;
+  top: 63px;
   left: 325px;
   z-index: 4;
   background-color: white;
   border: 1.6px solid black;
   border-radius: 50%;
-  padding: 5px;
-  padding-left: 6px;
+  // padding: 5px;
+  // padding-left: 6px;
   font-size: 1.2rem;
   box-shadow: 0 2px 8px rgba(0,0,0,0.1);
   display: flex;
@@ -33,8 +33,8 @@ const MapResetButton = styled.button`
 `;
 
 const MapResetIcon = styled.img`
-  width: 30px;
-  height: 30px;
+  width: 40px;
+  height: 40px;
   object-fit: contain;
 `;
 
@@ -47,7 +47,7 @@ function MapReset({ onReset }) {
 
   return (
     <MapResetButton onClick={handleClick} title="도시공상가로 돌아가기">
-      <MapResetIcon src="/icon/logo.png" alt="map-reset" />
+      <MapResetIcon src="/icon/indeogdo.png" alt="map-reset" />
     </MapResetButton>
   );
 }
